509f7a592adf053cd5be65ff2b6c283f57df8420
Jonas <jonaspietsch@live.de>
2017-06-07 14:14:44 +0200

    Neuer Name
    Namensänderung von classes.php zu constructor.php un Anpassung der Verknüofung in index.php.

---
+++ backend/php/constructor.php
@@ -0,0 +1,17 @@
+<?php
+
+class Constructor{
+
+  public $title;
+	public $headline;
+  public $viewfile	= '.default';
+	public $cssfiles	= [];
+	public $jsfiles		= [];
+
+  function __construct(argument){
+
+  }
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once "config.php";
-require_once __PHP__ . "/classes.php";
+require_once __PHP__ . "/constructor.php";
 
 ?>
